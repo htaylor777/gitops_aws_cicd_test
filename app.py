ef8785a1575d4c598d55c37748d846d1a7e38639
Hilery Taylor <47233288+htaylor777@users.noreply.github.com>
2022-10-23 16:55:32 -0700

    update route

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'What is up and down?'
+
+@app.route('/about/'):
+    return render_template('about.html')
