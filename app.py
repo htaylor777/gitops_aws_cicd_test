02a2f103078fce0ea89277db8a7af248463a98de
Hilery Taylor <hilerytaylor@gmail.com>
2022-10-23 00:51:48 -0700

    updated routes to test

--- app.py
+++ app.py
@@ -9,13 +9,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
-    
-@app.route("/larry")
-def larry():
-    return "Hello, LT!"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
