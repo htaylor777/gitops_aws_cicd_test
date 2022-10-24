48f3a45aeff3cfb68cdcd934eebb445dc000da30
Hilery Taylor <47233288+htaylor777@users.noreply.github.com>
2022-10-23 17:16:25 -0700

    check

--- app.py
+++ app.py
@@ -8,6 +8,3 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", message="Hello Flask!");   
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
