359fcc4eb5f075d1242f5822c6b40e43eff48f76
Hilery Taylor <hilerytaylor@gmail.com>
2022-10-23 23:22:44 -0700

    updated index file and static

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/services')
-def services():
-    return render_template('services.html') 
+@app.route('/demo2')
+def demo2():
+    return render_template('demo2.html') 
+
+@app.route('/demo3')
+def demo3():
+    return render_template('demo3.html')
+    
+@app.route('/demo4')
+def demo4():
+    return render_template('demo4.html')  
 
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index.html')      
     
-@app.route('/about')
-def about():
-    return render_template('about.html')      
