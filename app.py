4066b112cc88cbdb89ace88f7e035eed85ffff3c
Hilery Taylor <hilerytaylor@gmail.com>
2022-10-23 18:38:27 -0700

    added another template

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')    
