033922e10b87d5bafe8568a21fe97a7114dc996a
Hilery Taylor <47233288+htaylor777@users.noreply.github.com>
2022-10-23 18:03:05 -0700

    updated

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
-# __init__.py
-import time
+from flask import render_template
 
-from flask import Flask 
-import render_template
-import request
-import arrow
-import requests
-app = Flask(__name__)
 
 @app.route('/')
-def this_index():
-    return render_template('contact.html')
-return app
-
-@app.route('/hello')
-def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+@app.route('/index')
+def index():
+    users = [ 'Rosalia','Adrianna','Victoria' ]
+    return render_template('index.html', title='Welcome', members=users)
 
