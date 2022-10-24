6e552c94ccc67ac7d048ef7552efe0f74e7651a6
Hilery Taylor <47233288+htaylor777@users.noreply.github.com>
2022-10-23 17:02:31 -0700

    updated again

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'What is up and down?'
 
-@app.route('/about/'):
+@app.route('/about'):
     return render_template('about.html')
