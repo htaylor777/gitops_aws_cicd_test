96c0ea141a7a1cd7a3a0db0a2770dd0a926cef23
Hilery Taylor <47233288+htaylor777@users.noreply.github.com>
2022-10-23 17:10:20 -0700

    more updates

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import os
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'What is up and down?'
+@app.route("/")
+def index():
+    return render_template("index.html", message="Hello Flask!");   
 
-@app.route('/about'):
-    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
